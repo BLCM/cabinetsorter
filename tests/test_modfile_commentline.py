ee1bd3b1e7ed24d9edfa2c0f11a0b5ec74af7859
CJ Kucera <pez@apocalyptech.com>
2019-04-27 14:40:45 -0500

    Heh, a single method in here is now fully-tested

--- tests/test_modfile_commentline.py
+++ tests/test_modfile_commentline.py
@@ -12,10 +12,86 @@ class ModFileCommentTests(unittest.TestCase):
         """
         self.modfile = ModFile(0)
 
-    def test_add_comment_line_blank(self):
+    def test_blank(self):
         self.modfile.add_comment_line('')
         self.assertEqual(self.modfile.mod_desc, [])
 
-    def test_add_comment_line_single(self):
+    def test_single(self):
         self.modfile.add_comment_line('testing')
         self.assertEqual(self.modfile.mod_desc, ['testing'])
+
+    def test_strips(self):
+        for char in ['/', '#', ' ', "\n", "\r", "\t"]:
+            with self.subTest(char=char):
+                self.modfile = ModFile(0)
+                self.modfile.add_comment_line('{}testing{}'.format(char, char))
+                self.assertEqual(self.modfile.mod_desc, ['testing'])
+
+    def test_all_strips(self):
+        self.modfile.add_comment_line("/#\n\r\t testing/#\n\r\t")
+        self.assertEqual(self.modfile.mod_desc, ['testing'])
+
+    def test_two(self):
+        self.modfile.add_comment_line('testing')
+        self.modfile.add_comment_line('testing2')
+        self.assertEqual(self.modfile.mod_desc, ['testing', 'testing2'])
+
+    def test_double_empty(self):
+        self.modfile.add_comment_line('testing')
+        self.modfile.add_comment_line('')
+        self.modfile.add_comment_line('')
+        self.assertEqual(self.modfile.mod_desc, ['testing', ''])
+
+    def test_initial_ascii_art(self):
+        for char in ['_', '/', '\\', '.', ':', '|', '#', '~', ' ', "\t"]:
+            with self.subTest(char=char):
+                self.modfile = ModFile(0)
+                self.modfile.add_comment_line(char)
+                self.assertEqual(self.modfile.mod_desc, [])
+
+    def test_after_ascii_art_no_whitespace_or_comments(self):
+        for char in ['_', '\\', '.', ':', '|', '~']:
+            with self.subTest(char=char):
+                self.modfile = ModFile(0)
+                self.modfile.add_comment_line('testing')
+                self.modfile.add_comment_line(char)
+                self.assertEqual(self.modfile.mod_desc, ['testing', char])
+
+    def test_initial_ascii_art_all(self):
+        self.modfile.add_comment_line("_/\\.:| \t#~")
+        self.assertEqual(self.modfile.mod_desc, [])
+
+    def test_initial_ascii_art_after(self):
+        art = "_/\\.:|# \t~"
+        self.modfile.add_comment_line('testing')
+        self.modfile.add_comment_line(art)
+        self.assertEqual(self.modfile.mod_desc, ['testing', art])
+
+    def test_matched_title(self):
+        title = 'Mod Title'
+        self.assertEqual(self.modfile.mod_title, None)
+        self.modfile.add_comment_line(title, match_title=title)
+        self.assertEqual(self.modfile.mod_title, title)
+        self.assertEqual(self.modfile.mod_desc, [])
+
+    def test_close_title(self):
+        title = 'Mod Title'
+        self.assertEqual(self.modfile.mod_title, None)
+        self.modfile.add_comment_line(title, match_title='{}z'.format(title))
+        self.assertEqual(self.modfile.mod_title, title)
+        self.assertEqual(self.modfile.mod_desc, [])
+
+    def test_unmatched_title(self):
+        title = 'Mod Title'
+        self.assertEqual(self.modfile.mod_title, None)
+        self.modfile.add_comment_line(title, match_title='Totally Different')
+        self.assertEqual(self.modfile.mod_title, None)
+        self.assertEqual(self.modfile.mod_desc, [title])
+
+    def test_matched_title_not_first(self):
+        title = 'Mod Title'
+        self.assertEqual(self.modfile.mod_title, None)
+        self.modfile.add_comment_line('testing')
+        self.modfile.add_comment_line(title, match_title=title)
+        self.assertEqual(self.modfile.mod_title, None)
+        self.assertEqual(self.modfile.mod_desc, ['testing', title])
